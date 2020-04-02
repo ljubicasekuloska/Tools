3526081a51e287ac308cf10e592aaf08003f7076
ljubicasekuloska <ljubicasekuloska@gmail.com>
2020-04-02 12:52:03 +0200

    Add tests for Task 2 of Resistors

--- spec/resistors_spec.rb
+++ spec/resistors_spec.rb
@@ -1,32 +1,47 @@
+# frozen_string_literal: true
 
 RSpec.describe Tools::Resistors do
-
+  describe 'returns a two digit number for input of two colors' do
     it 'returns a two digit number for input of two colors' do
-      resistor = Tools::Resistors.new('Brown', 'Green')
-      expect(resistor.output).to eq(15)
+      resistor = Tools::Resistors.new(%w[Brown Green])
+      expect(resistor.base).to eq(15)
     end
 
     it 'returns a two digit number for input of two colors' do
-      resistor = Tools::Resistors.new('Black', 'Violet')
-      expect(resistor.output).to eq(07)
+      resistor = Tools::Resistors.new(%w[Black Violet])
+      expect(resistor.base).to eq(07)
     end
+
     it 'returns a two digit number for input of two colors' do
-      resistor = Tools::Resistors.new('Violet', 'Violet')
-      expect(resistor.output).to eq(77)
+      resistor = Tools::Resistors.new(%w[Violet Violet])
+      expect(resistor.base).to eq(77)
     end
 
     it 'ignores more than two colors for input' do
-      resistor = Tools::Resistors.new('Brown', 'Violet', 'Green')
-      expect(resistor.output).to eq(17)
+      resistor = Tools::Resistors.new(%w[Brown Violet Green])
+      expect(resistor.base).to eq(17)
     end
 
     it 'ignores more than two colors for input' do
-      resistor = Tools::Resistors.new('Orange', 'Blue', 'Black')
-      expect(resistor.output).to eq(36)
-    end
-
-    it 'ignores Gold and Silver as input' do
-      resistor = Tools::Resistors.new('Gold', 'Silver')
-      expect(resistor.output).to eq(0)
+      resistor = Tools::Resistors.new(%w[Orange Blue Black])
+      expect(resistor.base).to eq(36)
     end
   end
+  describe 'returns specification for resistance and tolerance' do
+    it 'returns 2700000 ohms +/- 5% for input red, violet, green and gold' do
+      resistor = Tools::Resistors.new(%w[red violet green gold])
+      expected = '2700000 ohms +/- 5%'
+      expect(resistor.specification).to eq expected
+    end
+    it 'returns 4300000000 ohms +/- 0.05% for input yellow, orange, gray and gray' do
+      resistor = Tools::Resistors.new(%w[yellow orange gold gray])
+      expected = '4.3 ohms +/- 0.05%'
+      expect(resistor.specification).to eq expected
+    end
+    it 'returns default tolerance value if there is no input for tolerance' do
+      resistor = Tools::Resistors.new(%w[brown violet red])
+      expected = '1700 ohms +/- 20%'
+      expect(resistor.specification).to eq expected
+    end
+  end
+end
